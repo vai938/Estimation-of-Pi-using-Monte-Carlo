e73852ad0c221c301bbe1ffeca5e3ec00e63d1ec
Vaibhav Thapliyal <94216240+vai938@users.noreply.github.com>
2022-08-12 19:12:11 +0530

    Update 2D_analysis.py

--- 2D_analysis.py
+++ 2D_analysis.py
@@ -20,9 +20,9 @@ for i in range(len(arr)):
        pi = 4 * (pin / ptot)
        sample.append(pi)
    empty.append(sample)
-column = ["min", "max", "mean", "stand-dev", "variance", "skewness", "kurtosis"]
+column = ["min", "max", "mean", "stand-dev", "variance", "skewness", "median"]
 stats_data = pd.DataFrame(data=np.zeros((7, 7)), columns=column, index=arr)
-min, max, mean, stdev, var, skew, kurt = [], [], [], [], [], [], []
+min, max, mean, stdev, var, skew, median = [], [], [], [], [], [], []
 for i in range(len(arr)):
    raw = pd.DataFrame(
        data=empty[i], columns={cols[i]}, index=np.arange(1, len(empty[i]) + 1)
@@ -33,7 +33,7 @@ for i in range(len(arr)):
    stdev.append(raw[str(cols[i])].std())
    var.append(raw[str(cols[i])].var())
    skew.append(raw[str(cols[i])].skew())
-   kurt.append(raw[str(cols[i])].kurtosis())
+   median.append(raw[str(cols[i])].median())
    sns.displot(data=raw, x=str(cols[i]), kind="kde")
    # raw.to_csv(str(cols[i]) + ".csv")
    del raw
@@ -43,7 +43,7 @@ stats_data["mean"] = mean
 stats_data["stand-dev"] = stdev
 stats_data["variance"] = var
 stats_data["skewness"] = skew
-stats_data["kurtosis"] = kurt
+stats_data["median"] = median
 print(stats_data)
 print(len(empty[1]))
 plt.show()
